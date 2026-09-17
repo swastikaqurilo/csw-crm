c6dd4c08b0d6982da2eabdf0885d3c89b7575a07
swastikaqurilo <swastikaqurilo@gmail.com>
2026-09-17 13:34:25 +0530

    MongoDB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
-    process.exit(1);
+    throw error;
   }
 };
 
